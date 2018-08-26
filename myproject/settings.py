1dec2cc3426ca8182939379eef5093d7fd016b58
bhasit2000 <bhasit2000@gmail.com>
2018-08-26 08:57:54 +0530

    done

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://bhasit2000.github.io/bhasit2000.github.io-todo_list/']
+#ALLOWED_HOSTS = ['']
 
 
 # Application definition
